189532c285f973999c85e470285fafd4644e322e
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-11 21:45:43 +0600

    problem one solved

---
+++ problemOne.js
@@ -0,0 +1,23 @@
+/*1. Number Comparison
+Write a program that takes a number as input and prints whether the number is positive negative, or zero.*/
+
+
+// Solve:
+
+var num = 65;
+
+if (num > 0) {
+    console.log("This is a positive number");
+}
+
+else if (num < 0) {
+    console.log("This is a negative number");
+}
+
+else if (num == 0) {
+    console.log("This is Zero");
+}
+
+else {
+    console.log("Please input a valid number");
+}
