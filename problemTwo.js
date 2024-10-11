ae91bed4879b6a0db12f5d6ae1e8e6d718d837c6
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-11 22:00:36 +0600

    problemTwo solve

---
+++ problemTwo.js
@@ -0,0 +1,35 @@
+// 2. Grade Calculator
+// Create a program that takes a student's score (out of 100) as input and assigns a grade based on the score.
+
+// A: 90 and above
+// B: 80 to 89
+// C: 70 to 79
+// D: 60 to 69
+// F: Below 60
+
+
+// solve:
+
+const result = Number('92');
+
+if (result >= 90 && result <= 100) {
+    console.log("You got A");
+}
+else if (result >= 80 && result <= 89) {
+    console.log("You got B");
+}
+else if (result >= 70 && result <= 79) {
+    console.log("You got C");
+}
+else if (result >= 60 && result <= 69) {
+    console.log("You got D");
+
+}
+
+else if (result < 60) {
+    console.log("You are fail");
+}
+else {
+    console.log("Please input a correct value");
+}
+
