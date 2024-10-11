f8e8183674784c1df66b3bf5a6142f1ba48147b6
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-11 23:44:05 +0600

    fivth problem solved

---
+++ problemFive.js
@@ -0,0 +1,24 @@
+// 5. Traffic Light System
+// Simulate a traffic light system where the user enters a light color (red, yellow, or green), and the program prints the appropriate action (stop, slow down, or go).
+
+
+// red=stop
+// yellow=slow down
+// green=go
+
+
+var input = String("GReeN");
+const color = input.toLowerCase();
+
+if (color == "red") {
+    console.log("Stop");
+}
+else if (color == "yellow") {
+    console.log("Slow Down");
+}
+else if (color == "green") {
+    console.log("Go");
+}
+else {
+    console.log("Please input the correct number");
+}
