0a2f2ca1438e4493f2b59998a03e2c973d324e4c
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-11 23:26:32 +0600

    fourth problem solved

---
+++ problemFour.js
@@ -0,0 +1,22 @@
+// 4. Even or Odd
+// Write a program that takes an integer as input and prints whether the number is even or odd.
+
+
+
+// solve:
+
+let a = '454.56';
+const num = parseInt(a);
+
+
+if (isNaN(num)) {
+    console.log("Please input a number");
+}
+else {
+    if (num % 2 == 0) {
+        console.log("This is an Even numnber");
+    }
+    else {
+        console.log("This is an Odd number");
+    }
+}
