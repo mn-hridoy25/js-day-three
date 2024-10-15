18de72427dba31aa291bb8bca6819e2af75972ff
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-15 18:49:44 +0600

    switch practice

---
+++ switchTwo.js
@@ -0,0 +1,21 @@
+
+
+
+const a = 30;
+const b = 15;
+var operator = '-';
+
+switch (operator) {
+    case '+':
+        console.log(a+b);
+        break;
+    case '-':
+        console.log(a-b);
+        break
+    case '*':
+        console.log(a*b);
+        break
+    case '/':
+        console.log(a/b);
+        break
+}
