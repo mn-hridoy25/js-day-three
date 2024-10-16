0b2f0261c83d890839fbbfdefbe61f58f840b53c
Mamunur Roshid <129681723+mn-hridoy25@users.noreply.github.com>
2024-10-16 16:31:57 +0600

    ternary operator added

---
+++ ternary.js
@@ -0,0 +1,9 @@
+// ternary operator ekdhoroner conditional statement. Er sompurno statement ke ek line e lekha hoy ebong kun variable er value hisebew rakha jai. Er structure:-
+// condition ? true side return : false side return
+
+
+// Example:-
+
+var n=20;
+
+const  
